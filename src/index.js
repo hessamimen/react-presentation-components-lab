37e3e251ae3156c486d4fe47e95f4f793e90366a
Hessam Imen <hessam.imen@gmail.com>
2022-05-18 19:38:24 +1000

    Done with assignmnet

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={(e) => {
+      e.target.innerHTML === 'I am just happy' ? e.target.innerHTML =  "I am just sad" : e.target.innerHTML = 'I am just happy'}} />
   </div>,
   document.getElementById('root')
 );
